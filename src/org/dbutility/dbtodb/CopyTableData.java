7071cda11cfc9d3764453b1d347219b32ab26e0f
antopaul@gmail.com <antopaul@gmail.com@5d802153-da9c-f854-b25d-b6ba5183cf38>
2012-10-18 13:00:48 +0000

    Added class to copy data from one table in one schema to another schema.

---
+++ src/org/dbutility/dbtodb/CopyTableData.java
@@ -0,0 +1,179 @@
+package org.dbutility.dbtodb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.dbutility.SQLBase;
+
+public class CopyTableData extends SQLBase {
+	
+	private static String CONFIG_FILE = "config/CopyTableData.properties";
+	
+	protected static String CURRENT_SQL = null; 
+	
+	protected static int EXIT_STATUS = 1;
+	
+	protected static String DATE = "DATE";
+	
+	protected static String TIMESTAMP = "TIMESTAMP";
+	
+	protected static String STRING = "STRING";
+	
+	protected static String TABLE = "table";
+	
+	protected Connection schema1Conn = null;
+	
+	protected Connection schema2Conn = null;
+	
+	protected Statement schema1Stmt = null;
+	
+	ResultSet schema1Rs = null;
+	
+	protected List<Integer> metadata = null;
+	
+	protected List<String> columnNames = null;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CopyTableData cmd = new CopyTableData();
+		cmd.execute();
+	}
+	
+	protected void execute() {
+		System.out.println("Executing CopyTableData...");
+		try {
+			loadConfig(CONFIG_FILE);
+			createConnections();
+			String tableName = config.getProperty(TABLE);
+			getTableData(tableName);
+			loadMetadata(schema1Rs);
+			insertData(tableName);
+			EXIT_STATUS = 0;
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			try {
+				rollback();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	protected void insertData(String tableName) {
+		StringBuilder sql = new StringBuilder();
+		StringBuilder values = new StringBuilder();
+		sql.append("INSERT INTO ").append(tableName);
+		sql.append("(");
+		boolean isFirst = true;
+		for(String column : columnNames) {
+			if(!isFirst) {
+				sql.append(",");
+				values.append(",");
+				
+			}
+			sql.append(column);
+			values.append("?");
+			isFirst = false;
+		}
+		
+		sql.append(")").append(" VALUES(").append(values).append(")");
+		
+		System.out.println("Insert SQL " + sql);
+		
+		Connection schema2Conn = getSchema2Connection();
+		PreparedStatement pstmt = null; 
+			
+		try {
+			pstmt = schema2Conn.prepareStatement(sql.toString());
+			
+			while(schema1Rs.next()) {
+				int index = 1;
+				for(Integer type : metadata) {
+					switch (type) {
+					case Types.DATE:
+						pstmt.setDate(index, schema1Rs.getDate(index));
+						break;
+					case Types.TIMESTAMP:
+						pstmt.setTimestamp(index, schema1Rs.getTimestamp(index));
+						break;
+					default:
+						pstmt.setString(index, schema1Rs.getString(index));
+					}
+					index++;
+				}
+				
+				pstmt.addBatch();
+			}
+			
+			int[] updateCount = pstmt.executeBatch();
+			
+			System.out.println("Inserted " + updateCount.length + " records");
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				commitOrRollback();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
+	
+	protected void getTableData(String tableName) {
+
+		try {
+			schema1Conn = getSchema1Connection();
+			schema1Stmt = schema1Conn.createStatement();
+			schema1Rs = schema1Stmt.executeQuery("SELECT * FROM " + tableName);
+		} catch(SQLException sqle) {
+			throw new RuntimeException(sqle);
+		} 
+	}
+	
+	protected List<Integer> loadMetadata(ResultSet rs) {
+		metadata = new LinkedList<Integer>(); 
+		columnNames = new LinkedList<String>();
+		
+		try {
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnCount = rsmd.getColumnCount();
+			
+			for(int i=1; i <= columnCount; i++) {
+				
+				columnNames.add(rsmd.getColumnName(i));
+				int type = rsmd.getColumnType(i);
+				
+				switch (type) {
+				case Types.DATE:
+					metadata.add(Types.DATE);
+					break;
+				case Types.TIMESTAMP:
+					metadata.add(Types.TIMESTAMP);
+					break;
+				default:
+					metadata.add(Types.VARCHAR);
+				}
+			}
+ 
+		} catch(SQLException sqle) {
+			throw new RuntimeException(sqle);
+		}
+		
+		System.out.println("Columns are : " + columnNames);
+		System.out.println("Types are : " + metadata);
+		
+		return metadata;
+	}
+}
